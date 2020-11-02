753f06f407e736295c695b0f8b729ba5d95bcd7c
tanayghosh2102 <tanay.ghosh.2102@gmail.com>
2020-11-02 17:43:22 -0500

    Comments added.

--- src/app/string-truncate.pipe.ts
+++ src/app/string-truncate.pipe.ts
@@ -7,13 +7,13 @@ export class StringTruncatePipe implements PipeTransform {
 
   transform(value: string, completeWords?: boolean, ellipsis?: string):string {
     let limit:number;
+    // Getting screen width to appropriately set the character limit.
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-    if(vw < 420) 
+    if(vw < 420) // Mobile
       limit = 21;
-    else if(vw > 420 && vw < 769)
+    else if(vw > 420 && vw < 769) // Table
       limit = 10;
-    else limit = 15;
+    else limit = 15; // Desktop
     console.log("limit is: ", limit);
     completeWords = (typeof completeWords === 'undefined') ? false : completeWords;
     ellipsis = (typeof ellipsis === 'undefined') ? '...' : ellipsis;
