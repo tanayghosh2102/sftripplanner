dbc3ce20527227f560bd5670a85d0b110223613a
tanayghosh2102 <tanay.ghosh.2102@gmail.com>
2020-11-02 17:25:45 -0500

    Tablet responsive fixes.

--- src/app/string-truncate.pipe.ts
+++ src/app/string-truncate.pipe.ts
@@ -5,8 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class StringTruncatePipe implements PipeTransform {
 
-  transform(value: string, limit?: number, completeWords?: boolean, ellipsis?: string):string {
-    limit = (typeof limit === 'undefined') ? 25 : limit;
+  transform(value: string, completeWords?: boolean, ellipsis?: string):string {
+    let limit:number;
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+    if(vw < 420) 
+      limit = 21;
+    else if(vw > 420 && vw < 769)
+      limit = 10;
+    else limit = 15;
+    console.log("limit is: ", limit);
     completeWords = (typeof completeWords === 'undefined') ? false : completeWords;
     ellipsis = (typeof ellipsis === 'undefined') ? '...' : ellipsis;
     if(completeWords) {
